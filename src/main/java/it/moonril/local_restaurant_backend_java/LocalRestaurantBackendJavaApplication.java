0154f6f58ad0bece8ce31b230d3e53f0238906cc
Vincenza <vinni2690@hotmail.com>
2026-02-02 12:01:26 +0100

    Initial commit

---
+++ src/main/java/it/moonril/local_restaurant_backend_java/LocalRestaurantBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package it.moonril.local_restaurant_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalRestaurantBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalRestaurantBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/moonril/local_restaurant_backend_java/LocalRestaurantBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.moonril.local_restaurant_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalRestaurantBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

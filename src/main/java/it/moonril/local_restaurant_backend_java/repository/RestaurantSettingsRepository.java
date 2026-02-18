64a4d5adf6ed09d92ed00dbef8d5b2939c4d4221
Vincenza <vinni2690@hotmail.com>
2026-02-18 10:21:31 +0100

    settings service

---
+++ src/main/java/it/moonril/local_restaurant_backend_java/repository/RestaurantSettingsRepository.java
@@ -0,0 +1,10 @@
+package it.moonril.local_restaurant_backend_java.repository;
+
+import it.moonril.local_restaurant_backend_java.models.Dish;
+import it.moonril.local_restaurant_backend_java.models.RestaurantSettings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface RestaurantSettingsRepository extends JpaRepository<RestaurantSettings, Integer>, PagingAndSortingRepository<RestaurantSettings, Integer> {
+
+}
--- src/main/java/it/moonril/local_restaurant_backend_java/service/RestaurantSettingsService.java
+++ src/main/java/it/moonril/local_restaurant_backend_java/service/RestaurantSettingsService.java
@@ -1,4 +1,70 @@
 package it.moonril.local_restaurant_backend_java.service;
 
+import it.moonril.local_restaurant_backend_java.dto.BookingDto;
+import it.moonril.local_restaurant_backend_java.dto.RestaurantSettingsDto;
+import it.moonril.local_restaurant_backend_java.enums.BookingStatus;
+import it.moonril.local_restaurant_backend_java.exceptions.NotFoundException;
+import it.moonril.local_restaurant_backend_java.models.Booking;
+import it.moonril.local_restaurant_backend_java.models.RestaurantSettings;
+import it.moonril.local_restaurant_backend_java.repository.RestaurantSettingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class RestaurantSettingsService {
+
+    @Autowired
+    private RestaurantSettingsRepository settingsRepository;
+
+    public RestaurantSettings saveSettings(RestaurantSettingsDto settingsDto) throws NotFoundException {
+        RestaurantSettings settings = new RestaurantSettings();
+
+        settings.setMaxCapacity(settingsDto.getMaxCapacity());
+        settings.setMaxPartySize(settingsDto.getMaxPartySize());
+        settings.setSlotDurationMinutes(settingsDto.getSlotDurationMinutes());
+        settings.setOpeningTime(settingsDto.getOpeningTime());
+        settings.setClosingTime(settingsDto.getClosingTime());
+        settings.setClosedDays(settingsDto.getClosedDays());
+        settings.setReservationAdvanceDays(settingsDto.getReservationAdvanceDays());
+        settings.setMinAdvanceMinutes(settingsDto.getMinAdvanceMinutes());
+        settings.setCancellationLimitHours(settingsDto.getCancellationLimitHours());
+
+        return settingsRepository.save(settings);
+    }
+
+    public RestaurantSettings getSettings(int id) throws NotFoundException{
+        return settingsRepository.findById(id).orElseThrow(() -> new NotFoundException("Settings with id: " + id + " not found"));
+    }
+
+
+    public RestaurantSettings updateSettings(int id, RestaurantSettingsDto settingsDto) throws NotFoundException {
+        RestaurantSettings settingsToUpdate = getSettings(id);
+
+        settingsToUpdate.setMaxCapacity(settingsDto.getMaxCapacity());
+        settingsToUpdate.setMaxPartySize(settingsDto.getMaxPartySize());
+        settingsToUpdate.setSlotDurationMinutes(settingsDto.getSlotDurationMinutes());
+        settingsToUpdate.setOpeningTime(settingsDto.getOpeningTime());
+        settingsToUpdate.setClosingTime(settingsDto.getClosingTime());
+        settingsToUpdate.setClosedDays(settingsDto.getClosedDays());
+        settingsToUpdate.setReservationAdvanceDays(settingsDto.getReservationAdvanceDays());
+        settingsToUpdate.setMinAdvanceMinutes(settingsDto.getMinAdvanceMinutes());
+        settingsToUpdate.setCancellationLimitHours(settingsDto.getCancellationLimitHours());
+
+
+
+
+        return settingsRepository.save(settingsToUpdate);
+    }
+
+
+
+
+    public void deleteSettings(int id) throws NotFoundException{
+        RestaurantSettings settingsToDelete = getSettings(id);
+
+        settingsRepository.delete(settingsToDelete);
+    }
 }
